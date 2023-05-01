463a135287225240d09d8950dc07e14bdeed5296
yjkimbc <132242614+yjkimbc@users.noreply.github.com>
2023-05-01 21:10:39 +0900

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,3 +1,96 @@
+/* 카드 템플릿. 새로 추가하는 로티를 맨 위에 배치하세요 */
+const template = document.getElementById("card-template");
+        const contents = document.querySelector(".contents");
+
+        const data = [
+            {
+                src: "https://jpassets.jobplanet.co.kr/production/uploads/material/media/11033/lottie_loading.json",
+                title: "lottie_loading.json"
+            },
+            {
+                src: "https://jpassets.jobplanet.co.kr/production/uploads/material/media/11034/lottie_spinner.json",
+                title: "lottie_spinner.json"
+            },
+            {
+              src: "https://jpassets.jobplanet.co.kr/production/uploads/material/media/11026/lottie_spinner_finish.json",
+              title: "lottie_spinner_finish.json"
+            },
+            {
+                src: "https://jpassets.jobplanet.co.kr/production/uploads/material/media/11028/lottie_navi_alarm.json",
+                title: "lottie_navi_alarm.json"
+            },
+            {
+              src: "https://jpassets.jobplanet.co.kr/production/uploads/material/media/11029/lottie_navi_compare.json",
+              title: "lottie_navi_compare.json"
+            },
+            {
+                src: "https://jpassets.jobplanet.co.kr/production/uploads/material/media/11030/lottie_navi_hire.json",
+                title: "lottie_navi_hire.json"
+            },
+            {
+              src: "https://jpassets.jobplanet.co.kr/production/uploads/material/media/11031/lottie_navi_my.json",
+              title: "llottie_navi_my.json"
+            },
+            {
+                src: "https://jpassets.jobplanet.co.kr/production/uploads/material/media/11032/lottie_navi_news.json",
+                title: "lottie_navi_news.json"
+            },
+            {
+              src: "https://jpassets.jobplanet.co.kr/production/uploads/material/media/11027/lottie_planet_loading.json",
+              title: "lottie_planet_loading.json"
+            },
+            {
+                src: "https://jpassets.jobplanet.co.kr/production/uploads/material/media/11019/lottie_magic.json",
+                title: "lottie_magic.json"
+            },
+            {
+                src: "https://jpassets.jobplanet.co.kr/production/uploads/material/media/10949/lottie_partypopper.json",
+                title: "lottie_partypopper.json"
+            },
+            {
+                src: "https://jpassets.jobplanet.co.kr/production/uploads/material/media/11017/lottie_appsplash.json",
+                title: "lottie_appsplash.json"
+            },
+            {
+                src: "https://jpassets.jobplanet.co.kr/production/uploads/material/media/11018/lottie_easypass_loading.json",
+                title: "lottie_easypass_loading.json"
+            },
+            {
+                src: "https://jpassets.jobplanet.co.kr/production/uploads/material/media/11020/lottie_match_simple.json",
+                title: "lottie_match_simple.json"
+            },
+            {
+                src: "https://jpassets.jobplanet.co.kr/production/uploads/material/media/11021/lottie_match.json",
+                title: "lottie_match.json"
+            },
+            {
+                src: "https://jpassets.jobplanet.co.kr/production/uploads/material/media/11022/lottie_mo_skeleton.json",
+                title: "lottie_mo_skeleton.json"
+            },
+            {
+                src: "https://jpassets.jobplanet.co.kr/production/uploads/material/media/11023/lottie_salary.json",
+                title: "lottie_salary.json"
+            },
+            {
+                src: "https://jpassets.jobplanet.co.kr/production/uploads/material/media/11025/lottie_salary_lock.json",
+                title: "lottie_salary_lock.json"
+            }
+        ];
+
+        data.forEach(item => {
+            const card = template.content.cloneNode(true);
+            card.querySelector("lottie-player").setAttribute("src", item.src);
+            card.querySelector(".title").textContent = item.title;
+            contents.appendChild(card);
+        });
+
+        function copy_to_clipboard(elem) {
+            const input = elem.previousElementSibling;
+            input.select();
+            document.execCommand("copy");
+        }
+
+/*주소 복사 기능*/
 function copy_to_clipboard(button) {
     const lottieSrc = button.parentNode.previousElementSibling.getAttribute('src');
     const textArea = document.createElement('textarea');
