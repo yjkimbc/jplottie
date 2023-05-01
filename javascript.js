47ea9b43e820c911c10c47e64553fa807201cefc
yjkimbc <132242614+yjkimbc@users.noreply.github.com>
2023-05-01 16:00:21 +0900

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,7 +1,10 @@
-function copy_to_clipboard() {    
-    var copyText = document.getElementById('myInput');
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    document.execCommand("Copy");
+function copy_to_clipboard(button) {
+    const lottieSrc = button.parentNode.previousElementSibling.getAttribute('src');
+    const textArea = document.createElement('textarea');
+    textArea.value = lottieSrc;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textArea);
     alert('주소를 복사했어요');
   }
